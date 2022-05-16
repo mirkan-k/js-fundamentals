30edfaf59e7e47bd8e1815e0156fb1893ecb2ace
mirkan-k <mirkilinc@gmail.com>
2022-05-16 18:48:21 +0100

    specsComplete

--- src/variables/declaration.js
+++ src/variables/declaration.js
@@ -2,7 +2,9 @@
 //
 //
 // TODO: Declare the variables firstName and age so that the tests pass
+let firstName = 'Jane'
 
+let age = 35
 // do not edit below this line
 let firstNameExport = ''
 try { firstNameExport = firstName } catch (e) {}
